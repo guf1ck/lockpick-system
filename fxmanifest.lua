301b78814807a2604cca1f2cdcec4fd56948c04d
Rafael Antunes <151884955+guf1ck@users.noreply.github.com>
2023-11-24 13:45:12 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,11 @@
 fx_version "adamant"
 games { "rdr3" }
 
-Owner 'ANTUNES#6541'
+Owner 'guf1ck, my discord'
 version '1.0'
-lua54 'yes'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
-
+lua54 'yes' -- cfx escrow
 ui_page 'html/index.html'
-
 files {
     'html/**/*'
 
